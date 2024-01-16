435c9b7463921336df58d1d0fd52fa4da333b2f4
Isakdl <isak.duner@gmail.com>
2024-01-16 16:20:33 +0100

    test: Add e2e test to validate relational objects are resolved when quering the db.

--- tests/serverpod_test_server/test_integration/database_operations/entity_relations/self/one_to_one/find/include_test.dart
+++ tests/serverpod_test_server/test_integration/database_operations/entity_relations/self/one_to_one/find/include_test.dart
@@ -7,14 +7,10 @@ void main() async {
   var session = await IntegrationTestServer().session();
 
   group('Given models with one to one relation', () {
-    tearDown(() async {
-      await Post.db.deleteWhere(session, where: (_) => db.Constant.bool(true));
-    });
+    late List<Post> posts;
 
-    test(
-        'when fetching models including relation then result includes relation data.',
-        () async {
-      var post = await Post.db.insert(
+    setUp(() async {
+      posts = await Post.db.insert(
         session,
         [
           Post(content: 'Hello world!'),
@@ -22,9 +18,17 @@ void main() async {
           Post(content: 'Hello a third time!'),
         ],
       );
-      await Post.db.attachRow.next(session, post[0], post[1]);
-      await Post.db.attachRow.next(session, post[1], post[2]);
+      await Post.db.attachRow.next(session, posts[0], posts[1]);
+      await Post.db.attachRow.next(session, posts[1], posts[2]);
+    });
 
+    tearDown(() async {
+      await Post.db.deleteWhere(session, where: (_) => db.Constant.bool(true));
+    });
+
+    test(
+        'when fetching models including relations then result includes relation data.',
+        () async {
       var postsFetched = await Post.db.find(
         session,
         include: Post.include(next: Post.include()),
@@ -35,35 +39,56 @@ void main() async {
       expect(postsFetched[1].next?.content, 'Hello a third time!');
       expect(postsFetched[2].next, isNull);
     });
-  });
-
-  group('Given models with nested one to one relations', () {
-    tearDown(() async {
-      await Post.db.deleteWhere(session, where: (_) => db.Constant.bool(true));
-    });
 
     test(
         'when fetching models including nested relation then result includes nested relation data.',
         () async {
-      var post = await Post.db.insert(
+      var postsFetched = await Post.db.findById(
         session,
-        [
-          Post(content: 'Hello world!'),
-          Post(content: 'Hello again!'),
-          Post(content: 'Hello a third time!'),
-        ],
-      );
-      await Post.db.attachRow.next(session, post[0], post[1]);
-      await Post.db.attachRow.next(session, post[1], post[2]);
-
-      var postsFetched = await Post.db.find(
-        session,
+        posts[0].id!,
         include: Post.include(next: Post.include(next: Post.include())),
-        orderBy: (t) => t.id,
       );
 
-      expect(postsFetched[0].next?.next?.content, 'Hello a third time!');
-      expect(postsFetched[1].next?.next?.content, isNull);
+      expect(postsFetched?.next?.next?.content, 'Hello a third time!');
+    });
+
+    test(
+        'when fetching models including nested relation then result has resolved the double linked dependency.',
+        () async {
+      var postsFetched = await Post.db.findById(
+        session,
+        posts[0].id!,
+        include: Post.include(next: Post.include(next: Post.include())),
+      );
+
+      expect(postsFetched?.next?.previous?.content, 'Hello world!');
+      expect(postsFetched?.next?.next?.previous?.content, 'Hello again!');
+    });
+
+    test(
+        'when fetching models including nested relation then result has resolved the double linked dependency in the backwards direction.',
+        () async {
+      var postsFetched = await Post.db.findById(
+        session,
+        posts[2].id!,
+        include: Post.include(previous: Post.include(previous: Post.include())),
+      );
+
+      expect(postsFetched?.previous?.next?.content, 'Hello a third time!');
+      expect(postsFetched?.previous?.previous?.next?.content, 'Hello again!');
+    });
+
+    test(
+        'when fetching models including nested relation then result has resolved the double linked dependency both backwards and forwards.',
+        () async {
+      var postFetched = await Post.db.findById(
+        session,
+        posts[1].id!,
+        include: Post.include(next: Post.include(), previous: Post.include()),
+      );
+
+      expect(postFetched?.next?.previous?.content, 'Hello again!');
+      expect(postFetched?.previous?.next?.content, 'Hello again!');
     });
   });
 }
